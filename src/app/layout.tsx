3ffa783bdca901a201275f2575257ca885f52f77
Yash Kumar <kryash90@gmail.com>
2023-08-14 10:33:45 +0530

    changees for prod -2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google';
 import Navbar from './Components/navbar/Navbar';
-import Footer from './Components/footer/Footer';
+import Footer from './Components/Footer/Footer';
 
 
 const inter = Inter({ subsets: ['latin'] })
